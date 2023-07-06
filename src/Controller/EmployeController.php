583f38870f6bd3623d2e62b4851c95469785909c
AnotheerDev <leger.julien94@gmail.com>
2023-07-06 19:14:24 +0200

    create controller

---
+++ src/Controller/EmployeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmployeController extends AbstractController
+{
+    #[Route('/employe', name: 'app_employe')]
+    public function index(): Response
+    {
+        return $this->render('employe/index.html.twig', [
+            'controller_name' => 'EmployeController',
+        ]);
+    }
+}
---
+++ src/Controller/EntrepriseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EntrepriseController extends AbstractController
+{
+    #[Route('/entreprise', name: 'app_entreprise')]
+    public function index(): Response
+    {
+        return $this->render('entreprise/index.html.twig', [
+            'controller_name' => 'EntrepriseController',
+        ]);
+    }
+}
